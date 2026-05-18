509ceebaa71cb30c62bbd3773396de11a36ddca0
igprvg <igprvg@gmail.com>
2026-05-19 00:42:42 +0300

    add rider plugin for godot

--- app/game/game.gd
+++ app/game/game.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+@onready var sprite_in: Sprite2D = $Sprite2D
+
 
 func _ready():
+	sprite_in.scale = Vector2(2, 2)
+
 	var units: Array[GoblinEnemy] = [
 		GoblinEnemy.new(GoblinEnemy.Type.MELEE),
 		GoblinEnemy.new(GoblinEnemy.Type.MELEE).with_level(2),
