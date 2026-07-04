547ed3e66db16e23a89b278e7da493ec7a86ff58
igprvg <igprvg@gmail.com>
2026-07-04 18:43:18 +0300

    update spawn script

--- app/elements/targets/default/default_target.gd
+++ app/elements/targets/default/default_target.gd
@@ -1,15 +1,15 @@
 class_name DefaultTarget
 extends CharacterBody2D
 
-const ITEM_MARGIN: float = PI / 6
+const ITEM_WIDTH: float = PI / 6
 const APPLE_POSITION: Vector2 = Vector2(0, 170)
-const MAX_DEPTH: int = 10
+const KNIFE_POSITION: Vector2 = Vector2(0, 185)
+const MAX_ATTEMPTS: int = 10
 
 @export var apple_spawn_count: int = 3
+@export var knife_spawn_count: int = 3
 
 var speed: float = PI
-var current_rng_position: float = 0.0
-var current_rng_position_step: int = 0
 var items_positions: Array[float] = []
 
 var knife_scene: PackedScene = preload("res://app/elements/knife/knife.tscn")
@@ -22,44 +22,50 @@ func _physics_process(delta: float) -> void:
 	rotation += speed * delta
 
 
-func is_entered_in_items_positions(item_position: float) -> bool:
+func is_entered_in_items_positions(item_position: float, rng_position: float) -> bool:
+	var half_item_width: float = ITEM_WIDTH / 2
+
 	return (
-		current_rng_position >= item_position - ITEM_MARGIN / 2
-		&& current_rng_position <= item_position + ITEM_MARGIN / 2
+		rng_position >= item_position - half_item_width
+		&& rng_position <= item_position + half_item_width
 	)
 
 
-func add_random_item_position() -> void:
-	if current_rng_position_step >= MAX_DEPTH:
+func add_random_item_position(current_attempt: int = 0) -> void:
+	if current_attempt >= MAX_ATTEMPTS:
 		return
 
-	current_rng_position = Globals.rng.randf_range(0, 2 * PI)
+	var current_rng_position = Globals.rng.randf_range(0, 2 * PI)
 
-	if items_positions.any(is_entered_in_items_positions):
-		current_rng_position_step += 1
-		add_random_item_position()
+	var is_found: bool = items_positions.any(
+		func(item_position: float) -> bool:
+			return is_entered_in_items_positions(item_position, current_rng_position)
+	)
+
+	if is_found:
+		add_random_item_position(current_attempt + 1)
 		return
 
 	items_positions.push_back(current_rng_position)
-	current_rng_position_step = 0
 
 
 func reset_items_positions() -> void:
 	items_positions.clear()
 
 
-func spawn_apple() -> void:
+func spawn_items(item_scene: PackedScene, item_scene_position: Vector2) -> void:
 	for _spawn_step in range(apple_spawn_count):
 		add_random_item_position()
 
 	for item_position in items_positions:
-		var apple: Apple = apple_scene.instantiate()
-		items_container.add_child(apple)
-		apple.position = APPLE_POSITION.rotated(item_position)
-		apple.rotation = item_position
+		var item: Node = item_scene.instantiate()
+		items_container.add_child(item)
+		item.position = item_scene_position.rotated(item_position)
+		item.rotation = item_position
 
 	reset_items_positions()
 
 
 func _ready() -> void:
-	spawn_apple()
+	spawn_items(apple_scene, APPLE_POSITION)
+	spawn_items(knife_scene, KNIFE_POSITION)
