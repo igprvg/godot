d950d5b9764eed6c3c52489313b1c700e4c873b2
igprvg <igprvg@gmail.com>
2026-07-05 12:19:05 +0300

    add knife collision with knife

--- app/elements/knife/knife.gd
+++ app/elements/knife/knife.gd
@@ -2,17 +2,34 @@ class_name Knife
 extends CharacterBody2D
 
 var speed: float = 4500.0
+var speed_away: float = 1450.0
+
 var is_moving: bool = false
+var is_moving_away: bool = false
+
+var away_rotation: float = 4 * PI
+var point_of_collision: Vector2 = Vector2.DOWN
 
 
 func _physics_process(delta: float) -> void:
 	if is_moving:
 		var collision: KinematicCollision2D = move_and_collide(Vector2.UP * speed * delta)
+
 		if collision:
 			var collider: Object = collision.get_collider()
+
 			if collider is DefaultTarget:
 				self.reparent(collider.items_container, true)
+
 				is_moving = false
+			if collider is Knife:
+				point_of_collision = collision.get_normal()
+
+				is_moving = false
+				is_moving_away = true
+	if is_moving_away:
+		rotation += away_rotation * delta
+		position += point_of_collision * speed_away * delta
 
 
 func throw() -> void:
